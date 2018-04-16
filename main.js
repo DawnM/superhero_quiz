3a85d25dfad92bd61dff40308f7a965179e7bae1
DawnM <dawn.cm@outlook.com>
2018-04-16 15:27:06 +0000

    refactor js to use functions

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
-alert('Welcome to Quiz Ninja!');
-
 const quiz = [
   ["What is Superman's real name?", "Clark Kent"],
   ["What is Wonder Woman's real name?", "Diana Prince"],
   ["What is Batman's real name?", "Bruce Wayne"]
 ];
 
-let score = 0; // initialise score
-
-// Quiz the user:
-for (const [question,answer] of quiz) {
-  const response = prompt(question);
-  if (response === answer) {
-    alert('Correct!');
-    score++;
-  } else {
-    alert(`Wrong! The correct answer is ${answer}`);
+function start(quiz) {
+  let score = 0;
+  
+  // main game loop
+  for (const [question, answer] of quiz) {
+    const response = ask(question);
+    check(response, answer);
   }
+  // end of main game loop
+  
+  gameOver();
+  
+  // ---------- function declarations ---------- 
+
+  function ask(question) {
+    return prompt(question);
+  }
+  
+  function check(response, answer) {
+    if (response === answer) {
+      alert('Correct!');
+      score++;
+    } else {
+      alert(`Wrong! The correct answer was ${answer}.`);
+    }
+  }
+  
+  function gameOver() {
+    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+  }
+  
+  // ---------- functions end ---------- 
 }
 
-// At the end of the game, report the player's score
-
-alert(`Game Over, you scored ${score} point${score != 1 ? 's' : ''}`);
-
-// const question = "What is Supermans real name?";
-// const answer = prompt(question);
-// alert(`You answered ${answer}`);
+start(quiz);
